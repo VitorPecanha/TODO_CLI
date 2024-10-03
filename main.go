d5d6e071506e045d975641bf19f4e455a4ec26b6
Vitor Peçanha <vitor.pecanha@hotmail.com>
2024-10-03 00:02:19 -0300

    Add basic tests

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 
 	todos.add("Buy meat")
 	todos.add("Buy bread")
 	todos.add("Learn Go")
-
-	fmt.Printf("%+v\n\n", todos)
-
-	todos.delete(0)
-	fmt.Printf("%+v\n\n", todos)
+	todos.toggle(0)
+	todos.print()
 }
